e62be3fce95f05f45b5649b91836c1cfa009767b
alitechznz <engineerbably@gmail.com>
2025-01-11 18:54:24 +0300

    alilaiii

--- routes/web.php
+++ routes/web.php
@@ -1,82 +1,12 @@
 <?php
 
-use App\Livewire\LGAComponent;
-use App\Livewire\PlanComponent;
-use App\Livewire\RoleComponent;
-use App\Livewire\UnitComponent;
-use App\Livewire\UserComponent;
-use App\Livewire\PillarComponent;
-use App\Livewire\ProjectProposal;
-use App\Livewire\RegionComponent;
-use App\Livewire\SectorComponent;
-use App\Livewire\ShehiaComponent;
-use App\Livewire\ProjectComponent;
-use App\Livewire\RdcUserComponent;
-use App\Livewire\SponsorComponent;
-use App\Livewire\DistrictComponent;
-use App\Livewire\MinistryComponent;
-use App\Livewire\UnitUserComponent;
-use App\Livewire\UnitValueComponent;
+
 use Illuminate\Support\Facades\Auth;
-use App\Livewire\AspirationComponent;
-use App\Livewire\BudgetTermComponent;
-use App\Livewire\DepartmentComponent;
-use App\Livewire\DisbursingComponent;
-use App\Livewire\MonitoringComponent;
-use App\Models\RequestImplementation;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\ConceptNoteComponent;
-use App\Livewire\InstitutionComponent;
-use App\Livewire\MinistryUserComponent;
-use App\Livewire\NationalPlanComponent;
-use App\Livewire\PriorityAreaComponent;
-use App\Livewire\RegionalPlanComponent;
 use App\Http\Controllers\HomeController;
-use App\Livewire\MonitoringEvaluationPlanComponent;
-use App\Livewire\ConceptNoteFormViewComponentw;
-use App\Livewire\DocumentDownloadComponent;
-use App\Livewire\LGAapproveListComponent;
-use App\Livewire\LGAChallengesComponent;
-use App\Livewire\MunicipalUserComponent;
-use App\Livewire\ResourcesListComponent;
-use App\Livewire\ScreeningListComponent;
-use App\Livewire\AppraisalListComponents;
-use App\Livewire\DepartmentUserComponent;
-use App\Livewire\LGAConceptNoteComponent;
-use App\Livewire\LGAProjectListComponent;
-use App\Livewire\ProjectProposalFormView;
-use App\Livewire\ConceptNoteEditComponent;
-use App\Livewire\ConceptNoteFormComponent;
-use App\Livewire\InstitutionUserComponent;
-use App\Livewire\ProjectCalenderComponent;
-use App\Livewire\ProjectProposalAppraisal;
-use App\Livewire\ProjectProposalDecission;
-use App\Livewire\ProjectQuestionComponent;
-use App\Livewire\ShehiaCommitteeComponent;
-use App\Livewire\SourceFinancingComponent;
-use App\Livewire\MunicipalCouncilComponent;
-use App\Livewire\ResourceTrackingComponent;
-use App\Livewire\AppraisalQuestionComponent;
-use App\Livewire\DomesticFinancingComponent;
-use App\Livewire\FinanceParticularComponent;
-use App\Livewire\InternationalPlanComponent;
-use App\Livewire\MEPlanSummaryListComponent;
-use App\Livewire\RegionalAuthorityComponent;
-use App\Livewire\FinancingAgreementComponent;
-use App\Livewire\AddResourceTrackingComponent;
-use App\Livewire\ConceptNoteApprovalComponent;
-use App\Livewire\ConceptNoteDecisionComponent;
-use App\Livewire\ProjectProposalFormComponent;
-use App\Livewire\ConceptNoteScreeningComponent;
-use App\Livewire\AddFinancingAgreementComponent;
-use App\Livewire\RequestImplementationComponent;
-use App\Livewire\Reports\ConceptNotePDFComponent;
-use App\Livewire\ViewImplementationRequestComponent;
-use App\Http\Controllers\ImplementationReportController;
-use App\Livewire\ManageExtensionComponent;
-use App\Livewire\RequestDisbursingComponent;
-use App\Livewire\RequestExtensionComponent;
-use App\Livewire\ViewRequestDisbursingComponent;
+use App\Http\Controllers\WeatherDataFeedComponent;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -124,7 +54,8 @@ Route::middleware('auth')->prefix('v1')->group(function () {
 
 
     #Concept note
-    // Route::get('forecast-list', ConceptNoteComponent::class)->name('forecast-list');
+    Route::get('forecast-list', ConceptNoteComponent::class)->name('forecast-list');
+    // Route::get('reporting-data', WeatherDataFeedComponent::class)->name('reporting-data');
 
     // Route::get('unit-values', UnitValueComponent::class)->name('unit-values');
     // Route::get('/concept-notes/{id}/view', [ConceptNoteComponent::class, 'view'])->name('concept-notes.view');
